e26fe5954ae8969aa2a46c4ec579cd836ca75167
Priya Bharmbe <priyabharambe70@gmail.com>
2021-03-30 14:48:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagenotfoundComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
